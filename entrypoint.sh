45a9d4ac93ee328aca39a33c5e5a20fe48c57502
Thomas Adi Putra <42173905+ThomasAdiPutra@users.noreply.github.com>
2025-09-16 10:08:33 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 if [ ! -f /app/index.php ]; then
     echo "[INFO] /app is empty. Downloading SLiMS..."
     
-    curl -L -o /tmp/slims.zip https://github.com/slims/slims9_bulian/releases/download/v9.6.1/slims9_bulian-9.6.1.zip
+    curl -L -o /tmp/slims.zip https://github.com/slims/slims9_bulian/releases/download/v9.7.1/slims9_bulian-9.7.1.zip
     
     echo "[INFO] Unzipping SLiMS..."
     unzip /tmp/slims.zip -d /tmp/slims
     
     echo "[INFO] Moving SLiMS files to /app..."
-    mv /tmp/slims/slims9_bulian-9.6.1/* /app
+    mv /tmp/slims/slims9_bulian-9.7.1/* /app
     rm -rf /tmp/slims /tmp/slims.zip
     
     echo "[INFO] Setting permissions for /app..."
